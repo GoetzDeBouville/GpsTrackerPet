74eedfcd122545f0d8fa19273bd0cd33fd48b8ba
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-11-19 16:11:51 +0300

    Добавлен bottom navigator. Подключен binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.hellcorp.gpstrackerpet"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hellcorp.gpstrackerpet"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/hellcorp/gpstrackerpet/MainActivity.kt
+++ app/src/main/java/com/hellcorp/gpstrackerpet/MainActivity.kt
@@ -2,10 +2,26 @@ package com.hellcorp.gpstrackerpet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.hellcorp.gpstrackerpet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        provideClickListeners()
+    }
+
+    private fun provideClickListeners() {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.item_home -> Log.e("MainActivity", "item_home")
+                R.id.item_tracks -> Log.e("MainActivity", "item_tracks")
+                R.id.item_settings -> Log.e("MainActivity", "item_settings")
+            }
+            true
+        }
     }
 }
