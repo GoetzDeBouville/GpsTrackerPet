dcb0ba5ef0b8e03a83a4b17571feaac28d5b93e3
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-11-21 20:28:33 +0300

    инициализирована карта на MainFragment

--- app/src/main/java/com/hellcorp/gpstrackerpet/fragments/MainFragment.kt
+++ app/src/main/java/com/hellcorp/gpstrackerpet/fragments/MainFragment.kt
@@ -1,11 +1,14 @@
 package com.hellcorp.gpstrackerpet.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hellcorp.gpstrackerpet.databinding.FragmentMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
@@ -15,6 +18,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        initOsm() // инициализация open street map обязательно до проприсовки фрагмента
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -24,7 +28,15 @@ class MainFragment : Fragment() {
         _binding = null
     }
 
+    private fun initOsm() {
+        Configuration.getInstance()
+            .load(requireContext(), activity?.getSharedPreferences(OSM_KEY, Context.MODE_PRIVATE))
+        Configuration.getInstance().userAgentValue = BuildConfig.LIBRARY_PACKAGE_NAME
+    }
+
     companion object {
+        private const val OSM_KEY = "osm_pref"
+
         @JvmStatic
         fun newInstance() = MainFragment()
     }
