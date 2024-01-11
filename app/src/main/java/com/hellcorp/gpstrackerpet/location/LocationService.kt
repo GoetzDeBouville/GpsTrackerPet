f7971b11504bda61b9d8556689ba87aff484dfa2
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-01-11 23:28:52 +0300

    Добавляет обновление местоположения с помощью LocationRequest

--- app/src/main/java/com/hellcorp/gpstrackerpet/location/LocationService.kt
+++ app/src/main/java/com/hellcorp/gpstrackerpet/location/LocationService.kt
@@ -1,17 +1,38 @@
 package com.hellcorp.gpstrackerpet.location
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.hellcorp.gpstrackerpet.MainActivity
 import com.hellcorp.gpstrackerpet.R
 
 class LocationService : Service() {
+    private lateinit var locationProvider: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private var locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            locationResult.lastLocation
+            Log.i("MyLog", "locationResult.lastLocation latitude = ${locationResult.lastLocation?.latitude} \nlongitude = ${locationResult.lastLocation?.longitude}")
+        }
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
@@ -19,12 +40,15 @@ class LocationService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         startNotification()
         isRuning = true
+        initLocation()
+        startLocationUpdate()
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
         isRuning = false
+        locationProvider.removeLocationUpdates(locationCallback)
     }
 
     private fun startNotification() {
@@ -55,6 +79,25 @@ class LocationService : Service() {
             .setContentIntent(pendingIntent)
             .build()
         startForeground(210, notification)
+    }
+
+    private fun initLocation() {
+        locationProvider = LocationServices.getFusedLocationProviderClient(baseContext)
+    }
+
+    private fun startLocationUpdate() {
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 3000)
+            .build()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) return
+        locationProvider.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()
+        )
     }
 
     companion object {
