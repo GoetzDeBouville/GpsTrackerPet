8c7d072386758b2311e934ebb0fcdaed334e2bd3
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-01-12 12:01:29 +0300

    Добавляет Parcelize зависимость. Добавляет подготовку модели LocationModel для отправки через BroadcastReciever

--- app/src/main/java/com/hellcorp/gpstrackerpet/location/LocationModel.kt
+++ app/src/main/java/com/hellcorp/gpstrackerpet/location/LocationModel.kt
@@ -1,11 +1,14 @@
 package com.hellcorp.gpstrackerpet.location
 
 import android.health.connect.datatypes.units.Velocity
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import org.osmdroid.util.Distance
 import org.osmdroid.util.GeoPoint
 
+@Parcelize
 data class LocationModel(
     val velocity: Float,
     val distance: Float,
     val geoPointList: ArrayList<GeoPoint>
-)
+) : Parcelable
--- app/src/main/java/com/hellcorp/gpstrackerpet/location/LocationService.kt
+++ app/src/main/java/com/hellcorp/gpstrackerpet/location/LocationService.kt
@@ -7,12 +7,14 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
@@ -21,15 +23,39 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.hellcorp.gpstrackerpet.MainActivity
 import com.hellcorp.gpstrackerpet.R
+import org.osmdroid.util.GeoPoint
 
 class LocationService : Service() {
+    private var distance = 0.0f
     private lateinit var locationProvider: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
+    private var lastLocation: Location? = null
+    private var geoPointsList = ArrayList<GeoPoint>()
     private var locationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             super.onLocationResult(locationResult)
-            locationResult.lastLocation
-            Log.i("MyLog", "locationResult.lastLocation latitude = ${locationResult.lastLocation?.latitude} \nlongitude = ${locationResult.lastLocation?.longitude}")
+            val currentLocation = locationResult.lastLocation
+            if (lastLocation != null && currentLocation != null) {
+//                if(currentLocation.speed > 0.3) { // TODO раскоментить для физического устройства
+//                    distance += lastLocation!!.distanceTo(currentLocation)
+//                }
+                geoPointsList.add(GeoPoint(currentLocation.latitude, currentLocation.longitude))
+                distance += lastLocation!!.distanceTo(currentLocation) // TODO удалить после роскомментирования условия
+                val locationModule = LocationModel(
+                    currentLocation.speed,
+                    distance,
+                    geoPointsList
+                )
+                sendLocationData(locationModule)
+            }
+            lastLocation = currentLocation
+            Log.i(
+                "MyLog",
+                "locationResult.lastLocation latitude = ${locationResult.lastLocation?.latitude} " +
+                        "\nlongitude = ${locationResult.lastLocation?.longitude} " +
+                        "\n altitude = ${locationResult.lastLocation?.altitude}"
+            )
+            Log.i("MyLog", "distance = $distance")
         }
     }
 
@@ -49,6 +75,12 @@ class LocationService : Service() {
         super.onDestroy()
         isRuning = false
         locationProvider.removeLocationUpdates(locationCallback)
+    }
+
+    private fun sendLocationData(locationModel: LocationModel) {
+        val intent = Intent(LOCATION_MODEL_INTENT)
+        intent.putExtra(LOCATION_MODEL_INTENT, locationModel)
+        LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
     }
 
     private fun startNotification() {
@@ -101,6 +133,7 @@ class LocationService : Service() {
     }
 
     companion object {
+        const val LOCATION_MODEL_INTENT = "location_intent"
         const val CHANEL_ID = "chanel_1"
         var isRuning = false
         var startTime = 0L
